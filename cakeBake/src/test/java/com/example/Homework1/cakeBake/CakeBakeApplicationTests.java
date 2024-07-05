732fcc65a56e5b93b5af6beacb6780ce674a1cc0
sharvi22 <124601873+sharvi22@users.noreply.github.com>
2024-07-04 20:20:41 -0500

    Project Setup

---
+++ cakeBake/src/test/java/com/example/Homework1/cakeBake/CakeBakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Homework1.cakeBake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeBakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
